395fd16066801e6e03b2c208b3f5199e12126e2a
Benjamin Schilling <benjamin.schilling33@gmail.com>
2025-05-11 15:31:14 +0200

    update to lvgl 9.2.2

--- lvgl_wemos_lolin32_joy-it_3_5.ino
+++ lvgl_wemos_lolin32_joy-it_3_5.ino
@@ -1,138 +1,101 @@
 #include <lvgl.h>
 #include <TFT_eSPI.h>
-#include <lv_examples.h>
-#include <Ticker.h>
+TFT_eSPI tft = TFT_eSPI();
 
-#define LVGL_TICK_PERIOD 20
+/*Set to your screen resolution and rotation*/
+#define TFT_HOR_RES   320
+#define TFT_VER_RES   480
+#define TFT_ROTATION  LV_DISPLAY_ROTATION_0
 
-TFT_eSPI tft = TFT_eSPI(); /* TFT instance */
-static lv_disp_buf_t disp_buf;
-static lv_color_t buf[LV_HOR_RES_MAX * 10];
-Ticker   lvgl_tick;             /* timer for lvgl */
+/*LVGL draw into this buffer, 1/10 screen size usually works well. The size is in bytes*/
+#define DRAW_BUF_SIZE (TFT_HOR_RES * TFT_VER_RES / 10 * (LV_COLOR_DEPTH / 8))
+uint32_t draw_buf[DRAW_BUF_SIZE / 4];
 
 #if LV_USE_LOG != 0
-/* Serial debugging */
-void my_print(lv_log_level_t level, const char * file, uint32_t line, const char * fn_name, const char * dsc)
+void my_print( lv_log_level_t level, const char * buf )
 {
-
-  Serial.printf("%s@%d->%s\r\n", file, line, dsc);
-  Serial.flush();
+    LV_UNUSED(level);
+    Serial.println(buf);
+    Serial.flush();
 }
 #endif
 
-/* Display flushing */
-void my_disp_flush(lv_disp_drv_t *disp, const lv_area_t *area, lv_color_t *color_p)
-{
-  uint32_t w = (area->x2 - area->x1 + 1);
-  uint32_t h = (area->y2 - area->y1 + 1);
-
-  tft.startWrite();
-  tft.setAddrWindow(area->x1, area->y1, w, h);
-  tft.pushColors(&color_p->full, w * h, true);
-  tft.endWrite();
-
-  lv_disp_flush_ready(disp);
-}
-
 /*Read the touchpad*/
-bool my_touchpad_read(lv_indev_drv_t *indev_driver, lv_indev_data_t *data)
+void my_touchpad_read( lv_indev_t * indev, lv_indev_data_t * data )
 {
   uint16_t touchX, touchY;
-
   bool touched = tft.getTouch(&touchX, &touchY, 600);
-
   if (!touched)
   {
-    data->state = LV_INDEV_STATE_REL;
-    return false;
+    data->state = LV_INDEV_STATE_RELEASED;
+    return;
   }
   else
   {
-    data->state = LV_INDEV_STATE_PR;
+    data->state = LV_INDEV_STATE_PRESSED;
   }
-
-  if (touchX > LV_HOR_RES_MAX || touchY > LV_VER_RES_MAX)
-  {
-    Serial.println("Y or y outside of expected parameters..");
-    Serial.print("y:");
-    Serial.print(touchX);
-    Serial.print(" x:");
-    Serial.print(touchY);
-  }
-  else
-  {
-    /*Set the coordinates*/
-    if (3 == tft.getRotation()) {
-      //Shift coordinates
-      data->point.x = LV_HOR_RES_MAX - touchX;
-      data->point.y = LV_VER_RES_MAX - touchY;
-    } else if (1 == tft.getRotation()) {
-      data->point.x = touchX;
-      data->point.y = touchY;
-    }
-
-    Serial.print("Data x");
-    Serial.println(touchX);
-
-    Serial.print("Data y");
-    Serial.println(touchY);
-  }
-
-  return false; /*Return `false` because we are not buffering and no more data to read*/
+  data->point.x = touchX;
+  data->point.y = touchY;
 }
 
-void lv_tick_handler() {
-  lv_tick_inc(LVGL_TICK_PERIOD);
+/*use Arduinos millis() as tick source*/
+static uint32_t my_tick(void)
+{
+    return millis();
+}
+
+static void event_handler(lv_event_t * e)
+{
+    lv_event_code_t code = lv_event_get_code(e);
+
+    if(code == LV_EVENT_CLICKED) {
+        LV_LOG_USER("Clicked");
+    }
+    else if(code == LV_EVENT_VALUE_CHANGED) {
+        LV_LOG_USER("Toggled");
+    }
 }
 
 void setup()
 {
-  Serial.begin(115200); /* prepare for possible serial debug */
+    Serial.begin(115200);
 
-  lv_init();
+    lv_init();
+    tft.init();
+    uint16_t calibrationData[5] = { 210, 3584, 306, 3551, 6 };
+    tft.setTouch(calibrationData);
 
+
+    /*Set a tick source so that LVGL will know how much time elapsed. */
+    lv_tick_set_cb(my_tick);
+
+    /* register print function for debugging */
 #if LV_USE_LOG != 0
-  lv_log_register_print_cb(my_print); /* register print function for debugging */
+    lv_log_register_print_cb( my_print );
 #endif
 
-  tft.begin(); /* TFT init */
-  tft.setRotation(0); /* Portrait orientation */
-
-  uint16_t calData[5] = { 305, 3542, 245, 3529, 3  };
-  tft.setTouch(calData);
-
-  lv_disp_buf_init(&disp_buf, buf, NULL, LV_HOR_RES_MAX * 10);
-
-  /*Initialize the display*/
-  lv_disp_drv_t disp_drv;
-  lv_disp_drv_init(&disp_drv);
-  disp_drv.hor_res = LV_HOR_RES_MAX;
-  disp_drv.ver_res = LV_VER_RES_MAX;
-  disp_drv.flush_cb = my_disp_flush;
-  disp_drv.buffer = &disp_buf;
-  lv_disp_drv_register(&disp_drv);
-
-  /*Initialize the (dummy) input device driver*/
-  lv_indev_drv_t indev_drv;
-  lv_indev_drv_init(&indev_drv);
-  indev_drv.type = LV_INDEV_TYPE_POINTER;
-  indev_drv.read_cb = my_touchpad_read;
-  lv_indev_drv_register(&indev_drv);
+    lv_display_t * disp;
+    /*TFT_eSPI can be enabled lv_conf.h to initialize the display in a simple way*/
+    disp = lv_tft_espi_create(TFT_HOR_RES, TFT_VER_RES, draw_buf, sizeof(draw_buf));
+    lv_display_set_rotation(disp, TFT_ROTATION);
 
 
-  lvgl_tick.attach_ms(LVGL_TICK_PERIOD, lv_tick_handler);
+    /*Initialize the (dummy) input device driver*/
+    lv_indev_t * indev = lv_indev_create();
+    lv_indev_set_type(indev, LV_INDEV_TYPE_POINTER); /*Touchpad should have POINTER type*/
+    lv_indev_set_read_cb(indev, my_touchpad_read);
 
-  /* Try an example from the lv_examples repository
-     https://github.com/lvgl/lv_examples*/
-  lv_ex_btn_1();
-  //printf();
+    /* Create a simple label
+     * ---------------------*/
+     lv_obj_t *label = lv_label_create( lv_screen_active() );
+     lv_label_set_text( label, "Hello Arduino, I'm LVGL!" );
+     lv_obj_align( label, LV_ALIGN_CENTER, 0, 0 ); 
 
+    Serial.println( "Setup done" );
 }
-
 
 void loop()
 {
-
-  lv_task_handler(); /* let the GUI do its work */
-  //delay(5);
+    lv_timer_handler(); /* let the GUI do its work */
+    delay(5); /* let this time pass */
 }
